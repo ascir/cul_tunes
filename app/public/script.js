0f8f2bcabdf1b1f892018513b5d810c8680f0b25
Anjana Ranjan <anjanar876@gmail.com>
2023-09-16 20:00:39 +1000

    API 2 complete

--- app/public/script.js
+++ app/public/script.js
@@ -4,11 +4,10 @@ var dish = document.getElementById("dish");
 var submitDishBtn = document.getElementById("submitDishBtn");
 var submitCuisineBtn = document.getElementById("submitCuisineBtn");
 const resultsContainer = document.getElementById("results");
-const loadingSpinner = document.querySelector('.spinner-border');
 const modal = document.getElementById('recipeModal');
 const modalInstructions = document.getElementById('recipeInstructions');
 const modalYoutubeLink = document.getElementById('recipeYoutubeLink');
-
+const loadingSpinner = document.querySelector('.spinner-border');
 
 let counterUpdated = false;
 console.log(counterUpdated);
@@ -48,7 +47,7 @@ function submitDish() {
                 }
             })
             .then(data => {
-                fetchDataAndPopulate(data);
+                PopulateApi1(data);
             })
             .catch(error => {
                 console.log(error);
@@ -59,15 +58,31 @@ function submitDish() {
 
 function submitCuisine() {
     userInput = cuisine.value.trim();
-    if (userInput == "--- Choose a Country ---") {
+    if (userInput == "--- Choose your Mood ---") {
         alert("Please choose an option.");
     }
     else {
-        alert("User chose: " + userInput)
+        resultsContainer.innerHTML = ''; 
+
+        fetch(`/api/explore/?area=${userInput}`)
+            .then((response) => {
+                if (response.status === 200) {
+                    return response.json();
+                } else {
+                    throw new Error("API request failed");
+                }
+            })
+            .then(data => {
+                PopulateApi2(data);
+            })
+            .catch(error => {
+                console.log(error);
+                resultsContainer.innerHTML = '<h4>No matches found ☹️</h4>';
+            });
     }
 }
 
-async function fetchDataAndPopulate(responseData) {
+async function PopulateApi1(responseData) {
     // Remove loading spinner
     if (loadingSpinner) {
         loadingSpinner.remove();
@@ -83,7 +98,7 @@ async function fetchDataAndPopulate(responseData) {
         row.classList.add('row');
 
         // Create and append recipe card
-        const recipeCard = createRecipeCard(data);
+        const recipeCard = api1RecipeCard(data);
         row.appendChild(recipeCard);
 
         // Create and append album cards
@@ -111,7 +126,61 @@ async function fetchDataAndPopulate(responseData) {
     });
 }
 
-function createRecipeCard(recipe) {
+async function PopulateApi2(responseData) {
+    // Remove loading spinner
+    if (loadingSpinner) {
+        loadingSpinner.remove();
+    }
+
+    // Create a jumbotron for recipes
+    const recipesJumbotron = document.createElement('div');
+    recipesJumbotron.classList.add('jumbotron');
+
+    const recipesTitle = document.createElement('h3');
+    recipesTitle.textContent = 'Recipes';
+    recipesJumbotron.appendChild(recipesTitle);
+
+    // Create a jumbotron for albums
+    const albumsJumbotron = document.createElement('div');
+    albumsJumbotron.classList.add('jumbotron');
+
+    const albumsTitle = document.createElement('h3');
+    albumsTitle.textContent = 'Music';
+    albumsJumbotron.appendChild(albumsTitle);
+
+    // Create rows for recipes and albums
+    const recipesRow = document.createElement('div');
+    recipesRow.classList.add('row');
+
+    const albumsRow = document.createElement('div');
+    albumsRow.classList.add('row');
+
+    responseData.combinedData.recipes.forEach((recipe) => {
+        // Create and append recipe card
+        const recipeCard = api2RecipeCard(recipe);
+        recipesRow.appendChild(recipeCard);
+    });
+
+    responseData.combinedData.albums.forEach((album) => {
+        // Create and append album card
+        const albumCard = createAlbumCard(album);
+        albumsRow.appendChild(albumCard);
+    });
+    
+    // Append rows to jumbotrons
+    recipesJumbotron.appendChild(recipesRow);
+    albumsJumbotron.appendChild(albumsRow);
+    
+    const recipesFooter = document.createElement('footer');
+    recipesFooter.textContent = 'If you want detailed instructions, enter the recipe of your choice into the input box';
+    recipesJumbotron.appendChild(recipesFooter);
+
+    // Append jumbotrons to the results container
+    resultsContainer.appendChild(recipesJumbotron);
+    resultsContainer.appendChild(albumsJumbotron);
+}
+
+function api1RecipeCard(recipe) {
     const card = document.createElement('div');
     card.classList.add('col-md-6', 'mb-3');
     card.innerHTML = `
@@ -126,13 +195,27 @@ function createRecipeCard(recipe) {
     return card;
 }
 
+function api2RecipeCard(recipe) {
+    const card = document.createElement('div');
+    card.classList.add('col-md-6', 'mb-3');
+    card.innerHTML = `
+        <div class="card">
+            <img src="${recipe.thumbnail}" class="card-img-top" alt="${recipe.meal} width="100" height="100">
+            <div class="card-body">
+                <p class="card-title">${recipe.meal}</p>
+            </div>
+        </div>
+    `;
+    return card;
+}
+
 // Function to create an album card
 function createAlbumCard(album) {
     const card = document.createElement('div');
     card.classList.add('col-md-3', 'mb-3', 'album-card');
     card.innerHTML = `
         <div class="card">
-            <img src="${album.image}" class="card-img-top" alt="${album.name}">
+            <img src="${album.image}" class="card-img-top" alt="${album.name} width="50" height="50"">
             <div class="card-body">
                 <h4 class="card-title">${album.name}</h4>
                 <h5 class="card-text">${album.artist}</h5>
